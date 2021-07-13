ef4ca90cdc1f050e020743358a580c8d89e78f16
milesdredd <87034849+milesdredd@users.noreply.github.com>
2021-07-13 20:51:13 +0530

    Add files via upload

---
+++ src/david.js
@@ -0,0 +1,22 @@
+import React from "react"
+import "./style.css"
+
+const David=()=>{
+    return(
+		<div>
+			<h1 style={{ textAlign : "center"}}> hey miles!</h1>
+			<button className="button"> red </button>
+			<button className="button"> pink</button>
+			<button className="button"> yellow </button>
+			
+		</div>
+    )
+	 
+}
+
+const miles=()=>{
+
+
+
+}
+export default David
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import David from "./david"
+
+
+ReactDOM.render( <David />, document.getElementById("root") )
+
